d68d189e93f91b2bd3b958cec8e78c0c49c94894
Jonatan Silva <jonatanda25@gmail.com>
2021-11-18 20:50:31 -0300

    limpando pasta e criando blog

--- receita/urls.py
+++ receita/urls.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-import receitas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('receitas/', include('receitas.urls')),
-    path('produto/', include('produto.urls')),
-    path('sobre/', include('sobre.urls')),
+    path('blog/', include('blog.urls')),
 ]
