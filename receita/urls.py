63361256cc37d7490b3232b1a92dbd37cc793319
Jonatan notebook <jonatanda25@gmail.com>
2021-11-19 09:47:45 -0300

    Criando sobre, home page e atualizacao note

--- receita/urls.py
+++ receita/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from home import views
 
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('sobre/', include('sobre.urls')),
 ]
